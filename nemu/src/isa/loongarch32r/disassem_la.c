c686136946aa7424b8079c5a768047e52bf9e637
tracer-ics2023 <tracer@njuics.org>
2024-04-24 04:42:07 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lzy-laptop 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:42:07 up  2:09,  1 user,  load average: 0.76, 0.41, 0.29

---
+++ nemu/src/isa/loongarch32r/disassem_la.c
@@ -0,0 +1,33 @@
+#include<disassem_la.h>
+/*****************************************************************************
+ * 
+ *  str:logbuf的指针
+ *  code是八进制表示的指令
+ *
+******************************************************************************/
+
+static void GetInst(char *inst, uint32_t code){
+    // uint32_t opcode_31_10 = BITS(code, 31, 10);
+    // uint32_t opcode_31_15 = BITS(code, 31, 15);
+    // uint32_t opcode_31_18 = BITS(code, 31, 18);
+    // uint32_t opcode_31_20 = BITS(code, 31, 20);
+    // uint32_t opcode_31_22 = BITS(code, 31, 22);
+    // uint32_t opcode_31_24 = BITS(code, 31, 24);
+    uint32_t opcode_31_25 = BITS(code, 31, 25);
+    // uint32_t opcode_31_26 = BITS(code, 31, 26);
+
+    if (opcode_31_25 == PCADDU12I) {
+        strcpy((char*)inst, "pcaddu12i");
+        return ;
+    }
+    else {
+        strcpy((char*)inst,"\0");
+        return;
+    }
+}
+void disassem_la(char *str, uint64_t pc, uint32_t code){
+        // uint8_t p[20];
+        char *inst = NULL;
+        GetInst(inst,code);
+        strcpy((char*)str,(char*)inst);
+}
