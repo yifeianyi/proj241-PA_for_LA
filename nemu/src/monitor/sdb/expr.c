6f786853decf9309d55eefa1263f42122732d7aa
tracer-ics2023 <tracer@njuics.org>
2024-04-25 22:41:19 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lemonpa-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:41:19 up 11:42,  1 user,  load average: 0.28, 0.27, 0.27

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -23,7 +23,7 @@
 enum {
   TK_NOTYPE = 256, TK_EQ, TK_NEQ,
   TK_NUMBER, TK_FLOAT, TK_IDENTIFIER,
-
+  TK_AND, TK_OR, TK_HEX, TK_REGISTER
 };
 
 static struct rule {
@@ -31,28 +31,28 @@ static struct rule {
   int token_type;
 } rules[] = {
 
-  {" +", TK_NOTYPE},    // spaces
+  {" +", TK_NOTYPE},
   {"\\+", '+'},
   {"\\-", '-'},
   {"\\*", '*'},
   {"\\(", '('},
   {"\\)", ')'},
   {"/", '/'},
-  {"==", TK_EQ},        // equal
-  {"!=", TK_NEQ},       // nequal
-
-  {"[0-9]+", TK_NUMBER},// 整数
-  {"[0-9]+\\.[0-9]+", TK_FLOAT}, // 浮点数
+  {"==", TK_EQ},
+  {"!=", TK_NEQ},
+  {"[0-9]+", TK_NUMBER},
+  {"[0-9]+\\.[0-9]+", TK_FLOAT},
   {"[a-zA-Z_][a-zA-Z0-9_]*", TK_IDENTIFIER}, // 标识符（变量名等）
+  {"&&", TK_AND},
+  {"\\|\\|", TK_OR},
+  {"0[xX][0-9a-fA-F]+", TK_HEX},
+  {"\\$[a-zA-Z]+[0-9]*", TK_REGISTER}, // reg
 };
 
 #define NR_REGEX ARRLEN(rules)
 
 static regex_t re[NR_REGEX] = {};
 
-/* Rules are used for many times.
- * Therefore we compile them only once before any usage.
- */
 void init_regex() {
   int i;
   char error_msg[128];
@@ -79,7 +79,6 @@ static bool make_token(char *e) {
   int position = 0;
   int i;
   regmatch_t pmatch;
-
   nr_token = 0;
 
   while (e[position] != '\0') {
@@ -88,16 +87,7 @@ static bool make_token(char *e) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
-
-        //Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-        //    i, rules[i].regex, position, substr_len, substr_len, substr_start);
-
         position += substr_len;
-
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
 
         switch (rules[i].token_type) {
           case TK_NOTYPE:
@@ -141,13 +131,10 @@ static bool check_parentheses(int p, int q) {
 
 int eval(int p, int q) {
     if (p > q) {
-        // 表达式为空，返回 0
         return 0;
     } else if (check_parentheses(p, q)) {
-        // 如果整个表达式被一对括号包围，则去掉括号继续递归计算
         return eval(p + 1, q - 1);
     } else {
-        // 查找最低优先级的运算符
         int level = 0, op = -1, i;
         for (i = q; i >= p; i--) {
             if (tokens[i].type == '(')
@@ -161,7 +148,6 @@ int eval(int p, int q) {
             }
         }
         if (op == -1) {
-            // 没有找到加减号，继续查找更低优先级的运算符
             for (i = q; i >= p; i--) {
                 if (tokens[i].type == '(')
                     level++;
@@ -176,13 +162,11 @@ int eval(int p, int q) {
         }
 
         if (op == -1) {
-            // 没有找到任何运算符，说明表达式是一个数值
             int val;
             sscanf(tokens[p].str, "%d", &val);
             return val;
         }
 
-        // 递归计算左右两边的表达式，并根据运算符进行计算
         int val1 = eval(p, op - 1);
         int val2 = eval(op + 1, q);
         switch (tokens[op].type) {
@@ -199,9 +183,6 @@ int eval(int p, int q) {
         }
     }
 }
-
-
-
 
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
