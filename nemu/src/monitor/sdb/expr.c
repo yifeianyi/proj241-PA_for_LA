cb86d8308e6a02cf4613ecc5156fa98020b6e3a8
tracer-ics2023 <tracer@njuics.org>
2024-04-28 01:52:59 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lemonpa-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:52:59 up 1 day,  6:54,  1 user,  load average: 0.46, 0.43, 0.45

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -218,7 +218,6 @@ int eval(int p, int q) {
         int val2 = eval(op + 1, q);
         switch (tokens[op].type) {
             case '+':
-              printf("+\n");
                 if ((val2 > 0 && (val1 > INT_MAX - val2 || val1 > INT_MAX)) || (val2 < 0 && (val1 < INT_MIN - val2 || val1 < INT_MIN))) {
                     printf("error: Integer overflow detected in addition\n");
                     return -1;
