bd3557b46a7e0d31a41869b478b6826f7e92d7c0
tracer-ics2023 <tracer@njuics.org>
2024-04-28 10:52:45 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lazy-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:52:45 up 30 min,  1 user,  load average: 0.71, 0.51, 0.43

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,10 +41,10 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
-  #ifdef CONFIG_WATCHPOINT
+  //#ifdef CONFIG_WATCHPOINT
   //scan watchpoint
   void check_watchpoint();
-  #endif
+  //#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
