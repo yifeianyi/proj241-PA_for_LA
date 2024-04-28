1bc231f4d9e4161fe6ec1ede118b99340dcca2ba
tracer-ics2023 <tracer@njuics.org>
2024-04-28 10:50:30 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lazy-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:50:30 up 28 min,  1 user,  load average: 0.40, 0.42, 0.40

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -26,12 +26,18 @@ const char *regs[] = {
 void isa_reg_display() {
   int length = sizeof(regs) / sizeof(regs[0]);
   for(int i = 0; i<length; i++){
-    printf("reg $%s ---> %d\n",regs[i],cpu.gpr[i]);
+    printf("reg $%s ---> 0x%08X\n",regs[i],cpu.gpr[i]);
   }
 
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  *success = true;
+  for(int i = 0;i<32;i++){
+    if(strcmp(s,regs[i]) == 0){
+      return cpu.gpr[i];
+    }
+  }
+  *success = false;
   return 0;
-  
 }
