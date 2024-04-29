1d3be944a1e158f42ee7e4b7271ca2f0365407df
tracer-ics2023 <tracer@njuics.org>
2024-04-29 11:08:36 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lemonpa-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:08:36 up  1:15,  1 user,  load average: 0.48, 0.51, 0.45

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -22,6 +22,7 @@ const char *regs[] = {
   "t4", "t5", "t6", "t7", "t8", "rs", "fp", "s0",
   "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", NULL
 };
+const int reg_len = ARRLEN(regs);
 
 void isa_reg_display() {
   int length = ARRLEN(regs);
