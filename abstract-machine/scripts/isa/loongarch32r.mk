208614e2fddd086f7999a46589cefff649ba25b4
tracer-ics2023 <tracer@njuics.org>
2024-04-29 18:22:48 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lzy-laptop 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:22:48 up 11:04,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/scripts/isa/loongarch32r.mk
+++ abstract-machine/scripts/isa/loongarch32r.mk
@@ -1,5 +1,4 @@
 CROSS_COMPILE := $(LA32R)/bin/loongarch32r-linux-gnusf-
 COMMON_FLAGS  := -fno-pic
-LA32R_INC     := -I/home/ysyx/Desktop/la32r-toolchains/install/lib/gcc/loongarch32r-linux-gnusf/8.3.0/include
 CFLAGS        += $(COMMON_FLAGS) -static 
 ASFLAGS       += $(COMMON_FLAGS) -O0
