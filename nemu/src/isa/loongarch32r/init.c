2ac07d6605708a8b4b0c2cf21e27adca0014c331
tracer-ics2023 <tracer@njuics.org>
2024-05-01 22:58:06 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lazy-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:58:06 up  7:16,  1 user,  load average: 0.49, 0.49, 0.46

--- nemu/src/isa/loongarch32r/init.c
+++ nemu/src/isa/loongarch32r/init.c
@@ -22,7 +22,7 @@ static const uint32_t img [] = {
   0x1c00000c,  // pcaddu12i $t0,0
   0x29804180,  // st.w $zero,$t0,16
   0x28804184,  // ld.w $a0,$t0,16
-  0x28000901,  
+  0x28400901,  
   0x002a0000,  // break 0 (used as nemu_trap)
   0xdeadbeef,  // some data
 };
