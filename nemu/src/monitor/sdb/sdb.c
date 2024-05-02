510ad3e584c89d896ef355c642afcdc1ff2129aa
tracer-ics2023 <tracer@njuics.org>
2024-05-03 03:25:39 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lemonpa-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:25:39 up 11:16,  1 user,  load average: 0.15, 0.19, 0.18

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -57,6 +57,9 @@ static int cmd_q(char *args) {
 extern const char* regs[];
 extern const int reg_len;
 bool contains_register(const char* input) {
+    if(strcmp(input,"pc")){
+      return true;
+    }
     for (size_t i = 0; i < reg_len; ++i) {
         if (strstr(input, regs[i]) != NULL) {
             return true;
