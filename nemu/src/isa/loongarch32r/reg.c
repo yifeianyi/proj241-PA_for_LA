2e418b8a37a3cd8b47c86ab2952872dc18f18395
tracer-ics2023 <tracer@njuics.org>
2024-05-03 03:32:35 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lemonpa-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:32:35 up 11:23,  1 user,  load average: 0.29, 0.23, 0.19

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -35,7 +35,7 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
   *success = true;
-  if(strcmp(s,"pc")){
+  if(strcmp(s,"$pc")){
     return cpu.pc;
   }
   for(int i = 0;i<32;i++){
