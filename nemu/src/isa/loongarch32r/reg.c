0e50ea3240fce9c256ccbba953ae5ccb962e2adf
tracer-ics2023 <tracer@njuics.org>
2024-05-03 03:21:10 +0800

    >  compile NEMU T202412059992867 run的全都队 Linux lemonpa-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:21:10 up 11:12,  1 user,  load average: 0.17, 0.19, 0.18

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -17,7 +17,7 @@
 #include "local-include/reg.h"
 
 const char *regs[] = {
-  "zero", "ra", "tp", "sp", "a0", "a1", "a2", "a3",
+  "0", "ra", "tp", "sp", "a0", "a1", "a2", "a3",
   "a4", "a5", "a6", "a7", "t0", "t1", "t2", "t3",
   "t4", "t5", "t6", "t7", "t8", "rs", "fp", "s0",
   "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8",
@@ -35,7 +35,7 @@ void isa_reg_display() {
 word_t isa_reg_str2val(const char *s, bool *success) {
   *success = true;
   for(int i = 0;i<32;i++){
-    if(strcmp(s,regs[i]) == 0){
+    if(strcmp(s,regs[i]) == 0 || strcmp(s,"pc")){
       return gpr(i);
     }
   }
