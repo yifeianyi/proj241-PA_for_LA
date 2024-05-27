9d8c639119b860f3e95143abdd21132606bae166
lemonsuqing <1428828189@qq.com>
2024-05-27 20:25:12 +0800

    实现ftrace

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -1,24 +1,10 @@
-/***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
-
 #include <common.h>
 
 extern uint64_t g_nr_guest_inst;
 
 #ifndef CONFIG_TARGET_AM
 FILE *log_fp = NULL;
+FILE *ftrace_fp = NULL;
 
 void init_log(const char *log_file) {
   log_fp = stdout;
@@ -34,4 +20,21 @@ bool log_enable() {
   return MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
          (g_nr_guest_inst <= CONFIG_TRACE_END), false);
 }
+
+void init_ftrace(const char *trace_file) {
+  ftrace_fp = stdout;
+  if (trace_file != NULL) {
+    FILE *fp = fopen(trace_file, "w");
+    Assert(fp, "Can not open '%s'", trace_file);
+    ftrace_fp = fp;
+  }
+  Log("Ftrace is written to %s", trace_file ? trace_file : "stdout");
+}
+
+bool ftrace_enable() {
+  bool enable = MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
+         (g_nr_guest_inst <= CONFIG_TRACE_END), false);
+  return enable;
+}
+
 #endif
