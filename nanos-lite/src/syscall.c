3be42e364d08d05e87aab078ba86288a1916c6d3
Li Zongyi <910002662@qq.com>
2024-08-13 04:28:23 +0800

    初步完成 strace

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,7 +5,14 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    case SYS_exit:  c->GPRx = 0; halt(c->GPRx); break;
+    case SYS_exit:{
+      c->GPRx = 0;
+#ifdef CONFIG_STRACE
+  printf("exit(code: 0x%x)\n",c->GPRx);
+#endif
+      halt(c->GPRx); 
+      break;
+    } 
     case SYS_yield: c->GPRx = 0; yield();       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
